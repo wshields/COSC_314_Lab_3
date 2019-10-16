eb98e2c5af9fa9187906d01f2eda7446b732af89
Will Shields <43360999+wshields@users.noreply.github.com>
2019-10-15 22:45:40 -0400

    Working boolean matrix creation

--- src/assignment/three/Driver.java
+++ src/assignment/three/Driver.java
@@ -1,8 +1,14 @@
 package assignment.three;
 
+import java.util.Scanner;
+
 public class Driver {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter an integer n");
+        int temp = keyboard.nextInt();
+        Matrix test = new Matrix(temp);
+        test.printMatrix();
     }
 }
--- src/assignment/three/Matrix.java
+++ src/assignment/three/Matrix.java
@@ -1,17 +1,48 @@
 package assignment.three;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Matrix {
     private int n;
-    private int[][] matrix;
+    private boolean[][] matrix;
 
     public Matrix(int n){
         this.n = n;
-        this.matrix = createMatrix(n);
+        this.matrix = createMatrix(this.n);
     }
 
 
+    public void printMatrix(){
+        for(int i = 0; i < matrix.length; i++){
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
 
-    private int[][] createMatrix(int n){
+    private boolean[][] createMatrix(int n){
+        boolean[][] ret = new boolean[n][n];
+        Scanner key = new Scanner(System.in);
+        System.out.println("Enter each row of the matrix one row at a time.");
+        for(int i = 0; i < n; i++){
+            String temp = key.nextLine();
+            boolean[] toPush = new boolean[n];
+            int count = 0;
+            for(int j = 0; j < temp.length(); j++){
+                if(temp.charAt(j) == '1'){
+                    toPush[count] = true;
+                    count++;
+                }else if(temp.charAt(j) == '0'){
+                    toPush[count] = false;
+                    count++;
+                }else {
 
+                }
+
+            }
+            for(int k = 0; k < toPush.length; k++){
+                ret[i][k] = toPush[k];
+            }
+        }
+        return ret;
     }
 }
