1c5aa793f56cba4b767311f3957065068bdced6a
Will Shields <43360999+wshields@users.noreply.github.com>
2019-10-15 21:52:17 -0400

    Initial file add

---
+++ src/assignment/three/Driver.java
@@ -0,0 +1,8 @@
+package assignment.three;
+
+public class Driver {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/assignment/three/Matrix.java
@@ -0,0 +1,17 @@
+package assignment.three;
+
+public class Matrix {
+    private int n;
+    private int[][] matrix;
+
+    public Matrix(int n){
+        this.n = n;
+        this.matrix = createMatrix(n);
+    }
+
+
+
+    private int[][] createMatrix(int n){
+
+    }
+}
